2d4112ce02be88cba70d8fd2f06957670ade2b62
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-02-03 17:03:24 -0300

    adicionando rotas e ligando html com fastapi

--- chatbot/api.py
+++ chatbot/api.py
@@ -4,4 +4,14 @@ import uvicorn
 
 app = FastAPI()
 
-template = Jinja2Templates(directory="templates")
+template = Jinja2Templates(directory="../frontend/templates")
+
+@app.get('/chat')
+def chatbot(req: Request):
+    return template.TemplateResponse(
+        name="chatbot.html",
+        context={"request":req}
+    )
+
+if __name__ == "__api__":
+    uvicorn.run("api:app")

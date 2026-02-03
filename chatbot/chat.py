34de2b8d05ec1630992dfa1471e7ebcfdec05de5
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-02-03 16:42:03 -0300

    criando chat

---
+++ chatbot/chat.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("OPENROUTER_API_KEY"),
+    base_url="https://openrouter.ai/api/v1"
+)
+
+def pegar_resposta(mensagem):
+    resposta = client.chat.completions.create(
+        model="stepfun/step-3.5-flash:free",
+        messages=[
+            {"role":"user","content":mensagem}
+        ]
+    )
+
+    return resposta.choices[0].message.content

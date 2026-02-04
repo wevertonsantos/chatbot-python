aa158dc008c69b2eaf810c7c5e7f8e8b4392c6ee
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-02-04 16:46:12 -0300

    fazendo back-end conversar com frontend

--- chatbot/api.py
+++ chatbot/api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI,Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from chat import resposta_bot
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,11 +12,14 @@ templates = Jinja2Templates(
     directory="frontend/templates"
 )
 
+class Mensagem(BaseModel):
+    mensagem: str
+
 @app.get('/',include_in_schema=False)
 @app.get('/chat',include_in_schema=False)
 def home_chatbot(request: Request):
     return templates.TemplateResponse(request,"chatbot.html")
 
 @app.post('/pegar_resposta')
-def pegar_resposta(mensagem:str):
-    return resposta_bot(mensagem)
+def pegar_resposta(dados: Mensagem):
+    return {"resposta": resposta_bot(dados.mensagem)}
--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,1 +1,25 @@
-console.log("Hello")
+const $ = document
+const botaoEnviarMensagemUsuario = $.getElementById("enviar_mensagem_usuario")
+
+botaoEnviarMensagemUsuario.addEventListener('click', (e) => {
+    e.preventDefault()
+    const mensagemUsuario = $.getElementById("mensagem_usuario").value
+
+    fetch("http://127.0.0.1:8000/pegar_resposta", {
+    method: 'POST',
+    headers: {
+        'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify({"mensagem":mensagemUsuario})
+    }).then(res => {
+        if (!res.ok) {
+            console.log('Problema')
+            return
+        }
+
+        return res.json()
+    })
+        .then(data => {
+        console.log(data.resposta)
+    })
+})
